bd219f49bf14f0948c24494a1b88b2f8495902cd
eramsay20 <eramsay10@gmail.com>
2020-05-15 10:48:39 -0400

    Initialize files for JSE2

---
+++ jsexercise2.js
@@ -0,0 +1,32 @@
+/* 
+1. Create a function ​printSequence t​ hat, given two numbers, returns a string of the values from the lowest number to the highest number.
+    a. printSequence(3, 8) → '3, 4, 5, 6, 7, 8'
+    b. printSequence(8, 3) → '3, 4, 5, 6, 7, 8'
+*/
+
+
+
+
+/* 
+2. Create a function ​calculate t​ hat, given a string and two numbers, does the following
+based on the value of the string:
+    a. 'add' → add the two numbers and return the result
+    b. 'subtract' → subtract the second number from the first and return the result
+    c. 'multiply' → multiply the numbers together and return the result
+    d. 'divide' → divide the first number by the second and return the result
+    e. For everything else, do not return anything.
+    */
+
+
+
+
+    
+/* 
+3. Create a function ​validate t​ hat takes in 2 strings and a number
+    a. Return false if either of the strings are undefined or empty (Hint: remember what
+    values are falsey)
+    b. Return false if the number is not undefined but is less than 0.
+    c. For all other cases, return true.
+    Stretch Requirements:
+1. Create a function that, given a number, returns true if it can be reached by the Fibonacci sequence, else return false.
+*/ 
