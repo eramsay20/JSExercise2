72eb4edcce39782e159a7c9b82e38a6b38d78376
eramsay20 <eramsay10@gmail.com>
2020-05-15 15:49:57 -0400

    JSE2 Q2 Completed

--- jsexercise2.js
+++ jsexercise2.js
@@ -45,7 +45,7 @@ printSequence(8,3); // 1.b
 // Q1 COMPLETE (ABOVE)
 
 /* 
-2. Create a function ​calculate t​ hat, given a string and two numbers, does the following
+2. Create a function ​calculate t​hat, given a string and two numbers, does the following
 based on the value of the string:
     a. 'add' → add the two numbers and return the result
     b. 'subtract' → subtract the second number from the first and return the result
@@ -53,13 +53,41 @@ based on the value of the string:
     d. 'divide' → divide the first number by the second and return the result
     e. For everything else, do not return anything.
     */
+// Q2 ANSWER BELOW //
+x = 1;
+y = 2;
+z = "";
+function calculate(x, y, z) {
+  
+    switch(z) {
+        case "add":
+            return x+y;
+            break;
+        case "subtract":
+            return x-y;
+            break;
+        case "multiply":
+            return x*y;
+            break;
+        case "divide":
+            return x/y;
+            break;
+        default:
+            return ;
+            break;
+    }
+}
+calculate (2,4,"add");
+
+// Q2 ANSWER COMPLETE //
+
 
 
 
 
     
 /* 
-3. Create a function ​validate t​ hat takes in 2 strings and a number
+3. Create a function ​validate t​hat takes in 2 strings and a number
     a. Return false if either of the strings are undefined or empty (Hint: remember what
     values are falsey)
     b. Return false if the number is not undefined but is less than 0.
